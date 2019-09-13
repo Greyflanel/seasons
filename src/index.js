10210ee1e4838acaed7ece79eb41a798ed50b150
Greyflanel <richardlovelace811@gmail.com>
2019-09-13 16:39:31 -0500

    Completed by extracting conditional render statements out of render method and created a function to hold them, and is called from the render method.

--- src/index.js
+++ src/index.js
@@ -22,15 +22,24 @@ class App extends React.Component {
       err => this.setState({ errorMessage: err.message })
     );
   }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage} </div>;
+      }
+      if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />
+      }
+      return <Spinner message="Please accept location request" />
+  }
+
   // Avoid doing anything besides returning JSX inside render method!!
   render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage} </div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-    return <Spinner message="Please accept location request" />;
+    return (
+        <div className="border red">
+           { this.renderContent()}
+        </div>
+    )
   };
 };
 
