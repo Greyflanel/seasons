c50529e67f4159dafc235831b5dfc7907742736f
Greyflanel <richardlovelace811@gmail.com>
2019-09-12 20:01:52 -0500

    created class component, defined constructor method, and initialized state object to contain our latitude property from geolocation.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
-const App = () => {
-window.navigator.geolocation.getCurrentPosition(
-(position) => console.log(position),
-(err) => console.log(err)
-);
+class App extends React.Component {
+constructor(props) {
+    super(props);
 
-    return <div>Hi there!</div>
+    this.state = { lat: null };
 }
 
-ReactDom.render(
-    <App/>,
-    document.querySelector('#root')
-);
+  render() {
+    window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
+    return <div> Latitude: </div>;
+  }
+}
+
+ReactDom.render(<App />, document.querySelector("#root"));
